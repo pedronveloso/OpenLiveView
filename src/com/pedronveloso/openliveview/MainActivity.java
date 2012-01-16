0ef8e49f5ecaf2bc55742ac00fa31cc4b1c2d1c2
Pedro Veloso <pedro@pedroT410.(none)>
2012-01-16 15:07:53 +0000

    first commit

---
+++ src/com/pedronveloso/openliveview/MainActivity.java
@@ -0,0 +1,166 @@
+package com.pedronveloso.openliveview;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.*;
+import java.nio.ByteOrder;
+import java.util.Set;
+import java.util.UUID;
+
+public class MainActivity extends Activity
+{
+    private static String LOG_TAG = "OpenLiveView";
+    BluetoothAdapter mBluetoothAdapter;
+    BluetoothDevice myLiveView;
+    TextView output;
+    
+    public void addToOuput(String line){
+        output.setText(output.getText()+"\n"+line);
+    }
+    
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        output = (TextView) findViewById(R.id.tv_output);
+        
+        if (ByteOrder.nativeOrder().equals(ByteOrder.BIG_ENDIAN))
+            addToOuput("Current platform byte order is: BigEndian");
+        else
+            addToOuput("Current platform byte order is: LittleEndian");
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            Log.e(LOG_TAG,"does not support bluetooth devices");
+            finish();
+        }
+
+        if (!mBluetoothAdapter.isEnabled()) {
+            Log.e(LOG_TAG, "bluetooth not enabled");
+            finish();
+        }
+
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        // If there are paired devices
+        if (pairedDevices.size() > 0) {
+            // Loop through paired devices
+            for (BluetoothDevice device : pairedDevices) {
+                // Add the name and address to an array adapter to show in a ListView
+                addToOuput(device.getName() + " : " + device.getAddress());
+                myLiveView = device;
+            }
+        }
+
+        ConnectThread con = new ConnectThread(myLiveView);
+        con.run();
+
+
+    }
+
+    private class ConnectThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final BluetoothDevice mmDevice;
+
+        public ConnectThread(BluetoothDevice device) {
+            // Use a temporary object that is later assigned to mmSocket,
+            // because mmSocket is final
+            BluetoothSocket tmp = null;
+            mmDevice = device;
+
+            // Get a BluetoothSocket to connect with the given BluetoothDevice
+            try {
+                // MY_UUID is the app's UUID string, also used by the server code
+                tmp = device.createRfcommSocketToServiceRecord(UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"));
+            } catch (IOException e) {
+                e.printStackTrace();
+                addToOuput("Fail to create RFCOMM");
+            }
+            mmSocket = tmp;
+        }
+
+        public void run() {
+            // Cancel discovery because it will slow down the connection
+            mBluetoothAdapter.cancelDiscovery();
+
+            try {
+                // Connect the device through the socket. This will block
+                // until it succeeds or throws an exception
+                mmSocket.connect();
+            } catch (IOException connectException) {
+                // Unable to connect; close the socket and get out
+                try {
+                    mmSocket.close();
+                } catch (IOException closeException) {
+                    addToOuput("close exception");
+                }
+                return;
+            }
+
+            // Do work to manage the connection (in a separate thread)
+            manageConnectedSocket(mmSocket);
+        }
+
+        /** Will cancel an in-progress connection, and close the socket */
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+                addToOuput("Fail on Cancel");
+            }
+        }
+    }
+
+
+    public void manageConnectedSocket(BluetoothSocket mmSocket){
+        addToOuput("reached manageConnectSocket");
+        DataInputStream tmpIn = null;
+        DataOutputStream tmpOut = null;
+        try {
+            tmpIn = new DataInputStream(mmSocket.getInputStream());
+            tmpOut = new DataOutputStream(mmSocket.getOutputStream());
+        } catch (IOException e) {
+            addToOuput("Failed to get In and/or Out stream(s)");
+            e.printStackTrace();
+        }
+
+
+        byte[] buffer = new byte[1024];  // buffer store for the stream
+        int bytes; // bytes returned from read()
+
+
+        try {
+            tmpOut.write(0x421010);
+        } catch (IOException e) {
+            e.printStackTrace();
+            addToOuput("FAIL TO WRITE");
+        }
+
+        while (true) {
+            try {
+                // Read from the InputStream
+                bytes = tmpIn.read(buffer);
+                // Send the obtained bytes to the UI Activity
+                addToOuput(Integer.toString(bytes));
+            } catch (IOException e) {
+                addToOuput("FAIL TO READ");
+                addToOuput(e.getMessage());
+                break;
+            }
+        }
+
+    }
+
+
+}
