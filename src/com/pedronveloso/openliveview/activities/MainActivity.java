e9cb36ffede984c22834f1f65ec3f0b8a4821a54
Pedro Veloso <pedro@pc2.(none)>
2012-01-28 12:48:28 +0000

    better development UI

--- src/com/pedronveloso/openliveview/activities/MainActivity.java
+++ src/com/pedronveloso/openliveview/activities/MainActivity.java
@@ -3,9 +3,13 @@ package com.pedronveloso.openliveview.activities;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -28,8 +32,10 @@ public class MainActivity extends Activity
 {
     BluetoothAdapter mBluetoothAdapter;
     TextView output;
-    Button btnVibrate;
-    Button btnLED;
+    private Button btnVibrate;
+    private Button btnLEDred;
+    private Button btnLEDgreen;
+    private Button btnLEDblue;
     
     public void addToOutput(final String line){
     	Log.d(Constants.LOG_TAG, line);
@@ -53,58 +59,20 @@ public class MainActivity extends Activity
 			is.read(StaticImages.staticIconAllEvents);
 			is.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} 
         
         
-        setContentView(R.layout.main);
+        setContentView(R.layout.test_activity);
+
+		TestActivityAdapter adapter = new TestActivityAdapter();
+        ViewPager myPager = (ViewPager) findViewById(R.id.testactivitypager);
+        myPager.setAdapter(adapter);
+        myPager.setCurrentItem(0);
+
+
 
         
-        output = (TextView) findViewById(R.id.tv_output);
-        
-        btnVibrate = (Button)findViewById(R.id.btnVibrate);
-        btnVibrate.setOnClickListener(this);
-        btnLED = (Button)findViewById(R.id.btnLED);
-        btnLED.setOnClickListener(this);
-        
-        if (ByteOrder.nativeOrder().equals(ByteOrder.BIG_ENDIAN))
-            addToOutput("Current platform byte order is: BigEndian");
-        else
-            addToOutput("Current platform byte order is: LittleEndian");
-
-        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (mBluetoothAdapter == null) {
-            Log.e(Constants.LOG_TAG,"does not support bluetooth devices");
-            finish();
-        }
-
-        if (!mBluetoothAdapter.isEnabled()) {
-            Log.e(Constants.LOG_TAG, "bluetooth not enabled");
-            finish();
-        }
-
-        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
-        BluetoothDevice liveView = null;
-        // If there are paired devices
-        if (pairedDevices.size() > 0) {
-            // Loop through paired devices
-            for (BluetoothDevice device : pairedDevices) {
-                // Add the name and address to an array adapter to show in a ListView
-                if ("LiveView".equals(device.getName())) {
-                	addToOutput(device.getName() + " : " + device.getAddress());
-                	liveView = device;
-                }
-            }
-        }
-
-        if (liveView != null) {
-        	BtServer.instance().setContext(this);
-        	BtServer.instance().setCallback(this);
-        	BtServer.instance().start(liveView);        	        	
-        }else{
-            addToOutput("Failed to obtain liveview device.");
-        }
 
     }
 
@@ -112,7 +80,9 @@ public class MainActivity extends Activity
     	if (isReady)
     		mBluetoothAdapter.cancelDiscovery();
     	btnVibrate.setEnabled(isReady);
-    	btnLED.setEnabled(isReady);
+    	btnLEDred.setEnabled(isReady);
+        btnLEDgreen.setEnabled(isReady);
+        btnLEDblue.setEnabled(isReady);
     	addToOutput("IsReadyChanged: "+isReady);
     }
 	
@@ -141,14 +111,116 @@ public class MainActivity extends Activity
 
 	public void onClick(View view) {
         switch (view.getId()){
-            case R.id.btnVibrate:
+            case R.id.btn_test_vibrate:
                 BtServer.instance().write(new VibrateRequest((short)0, (short)500));
                 break;
-            case R.id.btnLED:
+            case R.id.btn_test_led_red:
                 BtServer.instance().write(new LEDRequest(Color.RED, (short)0, (short)500));
+                break;
+            case R.id.btn_test_led_green:
+                BtServer.instance().write(new LEDRequest(Color.GREEN, (short)0, (short)500));
+                break;
+            case R.id.btn_test_led_blue:
+                BtServer.instance().write(new LEDRequest(Color.BLUE, (short)0, (short)500));
                 break;
         }
 	}
 
+    private class TestActivityAdapter extends PagerAdapter{
+        @Override
+        public boolean isViewFromObject(View arg0, Object arg1) {
+            return arg0 == ((View) arg1);
+        }
+
+        public Object instantiateItem(View collection, int position) {
+
+                    LayoutInflater inflater = (LayoutInflater) collection.getContext()
+                            .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+                    int resId = 0;
+                    switch (position) {
+                    case 0:
+                        resId = R.layout.test_activity_left;
+                        break;
+                    case 1:
+                        resId = R.layout.test_activity_right;
+                        break;
+                    }
+
+                    View view = inflater.inflate(resId, null);
+
+                    ((ViewPager) collection).addView(view, 0);
+
+                    // link to layout stuff
+                    if (position==0){
+                        output = (TextView) view.findViewById(R.id.tv_output);
+                    }
+                    if (position==1){
+                        btnVibrate = (Button) view.findViewById(R.id.btn_test_vibrate);
+                        btnVibrate.setOnClickListener(MainActivity.this);
+                        btnLEDred = (Button) view.findViewById(R.id.btn_test_led_red);
+                        btnLEDred.setOnClickListener(MainActivity.this);
+                        btnLEDgreen = (Button) view.findViewById(R.id.btn_test_led_green);
+                        btnLEDgreen.setOnClickListener(MainActivity.this);
+                        btnLEDblue = (Button) view.findViewById(R.id.btn_test_led_blue);
+                        btnLEDblue.setOnClickListener(MainActivity.this);
+
+                        //continue loading the app logic after UI has been instantiated
+                        continueLoading();
+                    }
+
+                    return view;
+                }
+
+        public int getCount() {
+            return 2;
+        }
+
+
+    }
+
+
+    /**
+     * Continue app execution after UI elements have been instantiated
+     */
+    private void continueLoading(){
+        if (ByteOrder.nativeOrder().equals(ByteOrder.BIG_ENDIAN))
+            addToOutput("Current platform byte order is: BigEndian");
+        else
+            addToOutput("Current platform byte order is: LittleEndian");
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            Log.e(Constants.LOG_TAG,"does not support bluetooth devices");
+            finish();
+        }
+
+        if (!mBluetoothAdapter.isEnabled()) {
+            Log.e(Constants.LOG_TAG, "bluetooth not enabled");
+            finish();
+        }
+
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        BluetoothDevice liveView = null;
+        // If there are paired devices
+        if (pairedDevices.size() > 0) {
+            // Loop through paired devices
+            for (BluetoothDevice device : pairedDevices) {
+                // Add the name and address to an array adapter to show in a ListView
+                if ("LiveView".equals(device.getName())) {
+                    addToOutput(device.getName() + " : " + device.getAddress());
+                    liveView = device;
+                }
+            }
+        }
+
+        if (liveView != null) {
+            BtServer.instance().setContext(this);
+            BtServer.instance().setCallback(this);
+            BtServer.instance().start(liveView);        	        	
+        }else{
+            addToOutput("Failed to obtain liveview device.");
+        }
+    }
 
 }
